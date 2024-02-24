cbe81967d806c297fc107c81a1e6ef94f9629a02
Tom Schwartz <thomas@languapps.com>
2024-02-24 17:28:50 +0100

    init

--- assets/js/firebaseInit.js
+++ assets/js/firebaseInit.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const db = getFirestore(app);
+window.db = db; // Assign Firestore instance to window.db
 // Export the app instance for use in other modules
 export { app };
