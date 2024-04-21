d548cb547721ba707e3f80e107e431ffe41e6962
Tom Schwartz <thomas@languapps.com>
2024-04-21 10:07:49 +0200

    new

--- assets/js/authentification.js
+++ assets/js/authentification.js
@@ -89,6 +89,27 @@ function signInWithFacebook() {
         });
 }
 
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 // Listen for authentication state changes
 onAuthStateChanged(auth, (user) => {
     if (user) {
